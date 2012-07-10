f3fd2bd35bf214b70ede82205f1c07ca8461fdf8
Ajay Nambi <anambi@codeaurora.org>
2012-07-10 08:51:36 -0700

    Telephony: Introduce DataProfile base class
    DataProfile is a base class from which we derive ApnSetting
    for GSM and DataProfileCdma for CDMA. This can be extended
    for other classes of DataProfiles.
    
    Change-Id: I61bddbe6e57baf44325264a7f255cf19c1119f81
    
    Conflicts:
    
    	src/java/com/android/internal/telephony/ApnContext.java
    	src/java/com/android/internal/telephony/DataConnectionTracker.java
    	src/java/com/android/internal/telephony/cdma/CdmaDataConnection.java
    	src/java/com/android/internal/telephony/cdma/CdmaDataConnectionTracker.java
    	src/java/com/android/internal/telephony/gsm/GsmDataConnection.java
    	src/java/com/android/internal/telephony/gsm/GsmDataConnectionTracker.java

--- src/java/com/android/internal/telephony/cdma/CdmaDataConnection.java
+++ src/java/com/android/internal/telephony/cdma/CdmaDataConnection.java
@@ -21,6 +21,7 @@ import android.util.Log;
 
 import com.android.internal.telephony.DataConnection;
 import com.android.internal.telephony.DataConnectionTracker;
+import com.android.internal.telephony.DataProfile;
 import com.android.internal.telephony.Phone;
 import com.android.internal.telephony.PhoneConstants;
 import com.android.internal.telephony.RILConstants;
@@ -74,6 +75,8 @@ public class CdmaDataConnection extends DataConnection {
         createTime = -1;
         lastFailTime = -1;
         lastFailCause = FailCause.NONE;
+
+        // TODO: The data profile's profile ID must be set when it is created.
         int dataProfile;
         if ((cp.apn != null) && (cp.apn.types != null) &&
                 (cp.apn.types.length > 0) &&
@@ -85,14 +88,16 @@ public class CdmaDataConnection extends DataConnection {
             dataProfile = RILConstants.DATA_PROFILE_DEFAULT;
         }
 
+        ((DataProfileCdma)mApn).setProfileId(dataProfile);
+
         // msg.obj will be returned in AsyncResult.userObj;
         Message msg = obtainMessage(EVENT_SETUP_DATA_CONNECTION_DONE, cp);
         msg.obj = cp;
         phone.mCM.setupDataCall(
                 Integer.toString(getRilRadioTechnology(RILConstants.SETUP_DATA_TECH_CDMA)),
-                Integer.toString(dataProfile),
-                null, null, null,
-                Integer.toString(RILConstants.SETUP_DATA_AUTH_PAP_CHAP),
+                Integer.toString(((DataProfileCdma)mApn).getProfileId()),
+                null, mApn.user, mApn.password,
+                Integer.toString(mApn.getAuthType()),
                 RILConstants.SETUP_DATA_PROTOCOL_IP, msg);
     }
 
