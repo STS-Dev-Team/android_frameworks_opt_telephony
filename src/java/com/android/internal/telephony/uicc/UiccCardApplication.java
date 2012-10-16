3358eecdd997bc3060380b1d90b3cf94e7d16269
Banavathu, Srinivas Naik <snb@codeaurora.org>
2012-10-16 12:28:10 +0530

    Telephony: Update the state of FDN Service correctly
    FDN Service state was derived based on the state of PIN2. Update the
    state of FDN service based on the FACILTY_LOCK messages instead.
    
    Telephony: Change the file path to FDN,MSISDN for USIM and CSIM
    Correcting the file path to EF_FDN and EF_MSISDN from Telecom DF to ADF
    for USIM and CSIM.
    
    Telephony: Correcting the file path to EF_ADN and EF_MSISDN
    This fix corrects the file path to EF_ADN and EF_MSISDN.
    
    Change-Id: I099698d0031f980c3883c17f3c170fbcadd7fb84

--- src/java/com/android/internal/telephony/uicc/UiccCardApplication.java
+++ src/java/com/android/internal/telephony/uicc/UiccCardApplication.java
@@ -56,6 +56,9 @@ public class UiccCardApplication {
     private boolean       mDesiredFdnEnabled;
     private boolean       mIccLockEnabled;
     private boolean       mDesiredPinLocked;
+    private boolean       mIccFdnAvailable = true; // Default is enabled.
+    private int mPin1RetryCount = -1;
+    private int mPin2RetryCount = -1;
 
     private CommandsInterface mCi;
     private Context mContext;
@@ -208,8 +211,16 @@ public class UiccCardApplication {
 
             int[] ints = (int[])ar.result;
             if(ints.length != 0) {
-                mIccFdnEnabled = (0!=ints[0]);
-                if (DBG) log("Query facility lock : "  + mIccFdnEnabled);
+                //0 - Available & Disabled, 1-Available & Enabled, 2-Unavailable.
+                if (ints[0] == 2) {
+                    mIccFdnEnabled = false;
+                    mIccFdnAvailable = false;
+                } else {
+                    mIccFdnEnabled = (ints[0] == 1) ? true : false;
+                    mIccFdnAvailable = true;
+                }
+                log("Query facility FDN : FDN service available: "+ mIccFdnAvailable
+                        +" enabled: "  + mIccFdnEnabled);
             } else {
                 loge("Bogus facility lock response");
             }
@@ -588,6 +599,29 @@ public class UiccCardApplication {
             return mIccFdnEnabled;
         }
     }
+
+    /**
+     * Check whether fdn (fixed dialing number) service is available.
+     * @return true if ICC fdn service available
+     *         false if ICC fdn service not available
+     */
+    public boolean getIccFdnAvailable() {
+        return mIccFdnAvailable;
+    }
+
+    /**
+     * @return No. of Attempts remaining to unlock PIN1/PUK1
+     */
+     public int getIccPin1RetryCount() {
+         return mPin1RetryCount;
+     }
+
+     /**
+      * @return No. of Attempts remaining to unlock PIN2/PUK2
+     */
+     public int getIccPin2RetryCount() {
+         return mPin2RetryCount;
+     }
 
     /**
      * Set the ICC pin lock enabled or disabled
